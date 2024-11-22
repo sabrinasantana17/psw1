49185e6973d012bf03bb91ebb95283eef36394fa
sabrinasantana17 <146476286+sabrinasantana17@users.noreply.github.com>
2024-11-22 20:02:59 -0300

    .

---
+++ atv02-sabrina-santana-19-11/index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>atv2</title>
+</head>
+<body>
+    <div class="header">
+    
+    <div id="div1">
+        <img src=".\imagens\marca-if-baiano-campus-guanambi-horizontal-removebg-preview.png" alt="imagem-if" class="img-header">
+    </div>
+    <div id="div2">
+        <h1>Formulário</h1>
+    </div>
+    <div id="div3"></div>
+
+    </div>
+
+    <nav class="nav">
+        <ul>
+        <li><a href="https://www.jotform.com/pt/?acc=main&adgroup=122011267414&matchtype=e&network=g&device=c&devicemodel=&extension_id=&feeditemid=&target_id=kwd-580074889601&target=&loc_interest=&loc_physical=1001522&placement=&utm_source=adwords&utm_campaign=13426539028&utm_medium=cpc&utm_term=criador%20de%20formul%C3%A1rios&utm_content=581313680130&utm_scp=&utm_cgroup=&gad_source=1&gclid=Cj0KCQiA0fu5BhDQARIsAMXUBOJzr8aKvFAUEdrMulqR_EHjzK-_xXR89pMTbQg-dveyVC0CrlMOJxEaArpnEALw_wcB">Jotform</a></li>
+            <li><a href="https://www.canva.com/pt_br/criar/formularios/">Cavan</a></li>
+            <li><a href="https://www.typeform.com/try/typeformbrand/?&tf_campaign=T2-Brand-Core-English-Combined_419251089&tf_source=bing&tf_medium=paid&tf_content=1322714863112329_&tf_term=typeform&tf_dv=c&tf_matchtype=e&tf_adposition=&tf_location=147566&msclkid=a6dca384c1b1130f36a0c4bbfc4ce215&gclid=a6dca384c1b1130f36a0c4bbfc4ce215&gclsrc=3p.ds">Typeform</a></li>
+            <li><a href="https://www.microsoft.com/en-us/microsoft-365/online-surveys-polls-quizzes">Microsoft</a></li>
+            <li><a href="https://br.hubspot.com/ofertas/demo/em983?utm_id=710168461900&utm_medium=paid&utm_source=google&utm_term=marketing_hubspot%20formularios_PT&utm_campaign=Marketing_MQLs_PT_LATAM_Brazil_Brand-HubSpot_e_c_campaignid1818014812_agid66937090982_google&utm_content=&hsa_ver=3&hsa_net=adwords&hsa_acc=2734776884&hsa_kw=hubspot%20formularios&hsa_grp=66937090982&hsa_mt=e&hsa_cam=1818014812&hsa_ad=710168461900&hsa_tgt=kwd-964138828065&hsa_src=g&gad_source=1&gclid=Cj0KCQiA0fu5BhDQARIsAMXUBOL0FFQYjwvXNpbHnCbWlSWOTcU02s2kX_AZo3jE5CHTnoCfR5bn1HIaAitsEALw_wcB">hubspot</a></li>
+            <li><a href="https://www.cognitoforms.com/">Cognito</a></li>
+           
+        </ul>
+    </nav>
+<br>
+
+<div class="corpo">
+    <div id="lado1"> </div>
+<div id="forms">
+    <h2>Crie uma conta</h2>
+    <form action="/action_page.php">
+    <label for="username">Username:</label><br>
+  <input type="text" placeholder="Digite aqui"><br>
+  <label for="pwd">Password:</label><br>
+  <input type="password" placeholder="Digite aqui"><br><br>
+  <label for="fname">Nome:</label><br>
+  <input type="text" placeholder="Digite aqui"><br>
+  <label for="lname">Endereço:</label><br>
+  <input type="text" placeholder="Digite aqui"><br><br>
+  <label for="fname">Bairro:</label><br>
+  <input type="text"placeholder="Digite aqui"><br><br>
+  <label for="datemin">Data de Nascimento:</label><br>
+  <input type="date" min="2000-01-02"><br><br>
+  <label for="phone">Telefone:</label><br>
+  <input type="tel" placeholder="(77) 876234590"><br><br>
+  <label for="email">Email:</label><br>
+  <input type="email" autocomplete="off" placeholder="santana21@gmail.com"><br><br>
+  <label for="phone">CPF:</label>
+  <input type="tel" placeholder="123.123.123-12"
+  pattern="[0-9]{3}-[0-9]{3}-[0-9]{2}"><br><br>
+  <label for="quantity">Idade:</label>
+  <input type="number" min="0" max="80" ><br>
+  <p>Sexo:</p>
+  <input type="radio">
+  <label for="html">Feminimo</label>
+ <input type="radio">
+ <label for="html">Masculino</label>
+<input type="radio">
+<label for="html">Não quero indentificar</label><br><br>
+<p>Você é fluente em quais línguas?</p>
+<input type="checkbox">
+  <label for="vehicle1">Inglês</label>
+  <input type="checkbox">
+  <label for="vehicle2">Espanhol</label>
+  <input type="checkbox">
+  <label for="vehicle3">Alemão</label>
+  <input type="checkbox">
+  <label for="vehicle3">Latim</label>
+  <input type="checkbox">
+  <label for="vehicle3">Outros</label><br><br>
+  <label for="favcolor">Selecione sua cor favorita:</label>
+  <input type="color" value="#ff0000"><br><br>
+  <label>Reside em qual estado brasileiro?</label>
+  <select>
+    <option>Acre</option>
+    <option>Amapá</option>
+    <option>Amazonas</option>
+    <option>Pará</option>
+    <option>Rondônia</option>
+    <option>Raraima</option>
+    <option>Tocatins</option>
+    <option>Alagoas</option>
+    <option>Bahia</option>
+    <option>Ceará</option>
+    <option>Maranhão</option>
+    <option>Paraíba</option>
+    <option>Pernambuco</option>
+    <option>Piauí</option>
+    <option>Rio Grande do Norte</option>
+    <option>Sergipe</option>
+    <option>Distrito Federal</option>
+    <option>Goiás</option>
+    <option>Mato Grosso</option>
+    <option>Mato Grosso do Sul</option>
+    <option>Espírito Santo</option>
+    <option>Minas Gerais</option>
+    <option>Rio de Janeiro</option>
+    <option>São Paulo</option>
+    <option>Paraná</option>
+    <option>Rio Grande do Sul</option>
+    <option>Santa Catarina</option>
+  </select><br><br>
+  <textarea name="message" rows="10" cols="30">Deixe aqui suas dúvidas!</textarea><br><br>
+  <button type="button" onclick="alert('Seja bem-vindo!')">Enviar</button>
+   <input type="reset" value="Reset">
+</form>
+
+</div>
+
+<div id="lado2"></div>
+</div>
+<br>
+<div id="rodape">
+        <p>&copy; 2024 Instituto Federal Baiano - Campus Guanambi. Todos os direitos reservados.</p>
+</div>
+
+</body>
+</html>
---
+++ aula-13-11/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>aula-13-11</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/buffet/src/index.php">buffet</a></li>
+            <li><a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/cineview/cineview/index.php">cineview</a></li>
+            <li><a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/livraria/index.php">livraria</a></li>
+            <li><a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/neurobiblioteca/">biblioteca</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
